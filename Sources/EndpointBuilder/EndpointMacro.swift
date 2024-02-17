9b8c5702a19b74cfa40a53a32b3d5ff5836ea2f0
Hal Lee <hal@lee.me>
2024-02-17 15:51:03 -0500

    Use macro to generate authorization property (#6)

--- Sources/EndpointBuilder/EndpointMacro.swift
+++ Sources/EndpointBuilder/EndpointMacro.swift
@@ -6,5 +6,5 @@ import RoutingKit
 /// Generates a `path` string for the given path components,
 /// along with a helper type for initializing any path parameters.
 @attached(extension, conformances: Endpoint)
-@attached(member, names: named(PathParameters), named(pathParameters), named(path))
+@attached(member, names: named(authorization), named(PathParameters), named(pathParameters), named(path))
 public macro Endpoint() = #externalMacro(module: "EndpointBuilderMacros", type: "EndpointMacro")
